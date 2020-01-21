33d8e9d1ce53aa2da9e98dffa65970a2879b791a
Jed Boyle <jboyle1@pca.ac.uk>
2020-01-21 11:21:10 +0000

    Issue with static files.

--- milestone_004/settings.py
+++ milestone_004/settings.py
@@ -165,9 +165,9 @@ STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Media Info:
 
